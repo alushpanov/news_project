d77f1284af511c737c8b908c4270d27e403dd462
Andrei Lushpanov <a.lushpanov@wis.software>
2021-03-04 15:23:27 +0300

    migrations made and applied

--- news/models.py
+++ news/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    news_text = models.TextField()
+    created_on = models.DateField(auto_now_add=True)
+    # image = models.FilePathField
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
