01cb3b9fe38806143111347f5fbb78ff1a20f17a
Andrei Lushpanov <a.lushpanov@wis.software>
2021-03-04 16:00:45 +0300

    __str__ overriden in models

--- news/models.py
+++ news/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
@@ -11,3 +14,6 @@ class Article(models.Model):
     created_on = models.DateField(auto_now_add=True)
     # image = models.FilePathField
     author = models.ForeignKey(Author, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
