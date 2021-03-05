35bdd76814499ada461ce19907ad5bc8a6793d8e
Andrei Lushpanov <a.lushpanov@wis.software>
2021-03-05 10:53:57 +0300

    base template removed

--- news_project/settings.py
+++ news_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'news_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['news_project/templates/'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
