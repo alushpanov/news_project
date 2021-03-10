11e112af6c2a614e60d7da75ebcd5f254f2ec420
Andrei Lushpanov <a.lushpanov@wis.software>
2021-03-10 16:41:34 +0300

    login, logout, register functions added

--- my_auth/urls.py
+++ my_auth/urls.py
@@ -4,4 +4,7 @@ from my_auth import views
 app_name = 'my_auth'
 urlpatterns = [
     path('', views.index_view, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.my_login, name='login'),
+    path('logout/', views.my_logout, name='logout'),
 ]
--- news_project/urls.py
+++ news_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('my_auth.urls')),
-    path('auth/', include('django.contrib.auth.urls')),
+    # path('auth/', include('django.contrib.auth.urls')),
 ]
