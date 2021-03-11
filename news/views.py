b73cdc38a638fcab30a6a59eeef654cd4819862a
Andrei Lushpanov <a.lushpanov@wis.software>
2021-03-11 17:08:34 +0300

    static and template dirs refactored, base template added, detail page removed

--- news/views.py
+++ news/views.py
@@ -5,12 +5,7 @@ from news.models import Article
 
 
 class IndexView(generic.ListView):
-    template_name = 'index.html'
+    template_name = 'news/index.html'
 
     def get_queryset(self):
         return Article.objects.all().order_by('-created_on')
-
-
-class DetailView(generic.DetailView):
-    model = Article
-    template_name = 'detail.html'
--- news_project/settings.py
+++ news_project/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'news_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,5 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # AUTH_USER_MODEL = 'User'
