b8a17f8456681e25196f0d762d75524ace0bfb3b
Andrei Lushpanov <a.lushpanov@wis.software>
2021-03-11 10:33:31 +0300

    google auth added

--- news_project/settings.py
+++ news_project/settings.py
@@ -148,3 +148,9 @@ SITE_ID = 2
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Settings for allauth telling Django to use email instead of username
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
