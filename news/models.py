4192b0f4cb8a2c03d0e764918bf1963335e06bf1
Andrei Lushpanov <a.lushpanov@wis.software>
2021-03-12 14:02:52 +0300

    relation moved to Article

--- news/models.py
+++ news/models.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 from news_project import settings
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
     article_text = models.TextField()
@@ -12,14 +19,7 @@ class Article(models.Model):
     image = models.ImageField('', null=True)
     # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, related_name='articles')
 
     def __str__(self):
         return self.title
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=20)
-    articles = models.ManyToManyField(Article, related_name='categories')
-
-    def __str__(self):
-        return self.name
