8e6e4e3ff1e7d2fcc6b9bd0877da501428d970be
Andrei Lushpanov <a.lushpanov@wis.software>
2021-03-12 12:01:24 +0300

    model Category added

--- news/admin.py
+++ news/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from news.models import Article
+from news.models import Article, Category
 
 admin.site.register(Article)
+admin.site.register(Category)
--- news/models.py
+++ news/models.py
@@ -1,4 +1,3 @@
-# import os
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,10 +7,19 @@ from news_project import settings
 class Article(models.Model):
     title = models.CharField(max_length=100)
     article_text = models.TextField()
-    created_on = models.DateField()
-    # image = models.FilePathField(path=os.path.join(settings.BASE_DIR))
+    created_on = models.DateField()  # auto_now_add=True
+    last_modified = models.DateField(auto_now=True)
+    image = models.ImageField('', null=True)
     # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    articles = models.ManyToManyField(Article, related_name='categories')
+
+    def __str__(self):
+        return self.name
