562e2f57af6055d1df437199caec72bc31518ca0
Andrei Lushpanov <a.lushpanov@wis.software>
2021-03-12 16:51:11 +0300

    categories limit added to admin form

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,21 @@
+from django import forms
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+
 from news.models import Article, Category
 
-admin.site.register(Article)
+
+class MyArticleAdminForm(forms.ModelForm):
+    def clean_categories(self):
+        data = self.cleaned_data['categories']
+        if len(data) > 3:
+            raise ValidationError('No more than 3 categories allowed!')
+        return data
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    form = MyArticleAdminForm
+
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Category)
