d860da56ce1a7d166fffbbcd8ac6ec1d4b2c6eec
Andrei Lushpanov <a.lushpanov@wis.software>
2021-03-18 15:16:28 +0300

    view class decorated

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from news import views
 
 app_name = 'news'
 urlpatterns = [
-    path('', login_required(views.IndexView.as_view()), name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,10 +1,16 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views import generic
 
 from news.models import Article
 
 
+@method_decorator(login_required, name='dispatch')
 class IndexView(generic.ListView):
     template_name = 'news/index.html'
 
     def get_queryset(self):
         return Article.objects.all().order_by('-created_at')
+
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
