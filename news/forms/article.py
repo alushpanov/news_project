c391f727a6e15be23e64cec6de04317ef946e5c0
Andrei Lushpanov <a.lushpanov@wis.software>
2021-03-18 12:43:40 +0300

    migrations reset, forms refactored

---
+++ news/forms/article.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from news.models import Article
+
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'text', 'categories', 'image']
+        widgets = {
+            'title': forms.TextInput(
+                attrs={"placeholder": "Title"}
+            ),
+            'text': forms.Textarea(
+                attrs={"placeholder": "Text"}
+            ),
+        }
+
+    def clean_categories(self):
+        data = self.cleaned_data.get('categories')
+        if len(data) > 3:
+            raise ValidationError('No more than 3 categories allowed!')
+        return data
+
+
+class ArticleAdminForm(ArticleForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'text', 'categories', 'image', 'author']
