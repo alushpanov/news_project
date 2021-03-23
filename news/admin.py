7930437433c3f54069d8027a03da39e975f3796d
Andrei Lushpanov <a.lushpanov@wis.software>
2021-03-23 18:07:49 +0300

    admin displays archived too

--- news/admin.py
+++ news/admin.py
@@ -7,6 +7,10 @@ from news.models import Article, Category
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     form = ArticleAdminForm
+    list_display = ('title', 'archived')
+
+    def get_queryset(self, request):
+        return Article.all_articles.order_by('-created_at')
 
 
 @admin.register(Category)
