e1404aa2502e8dbb0cc414a8afc7f7167fce04b5
Andrei Lushpanov <a.lushpanov@wis.software>
2021-03-29 16:11:56 +0300

    draft search added

--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.utils.decorators import method_decorator
 from django.views import generic
@@ -51,3 +52,21 @@ def archive_article(request, pk):
     article.archived = True
     article.save()
     return redirect('news:user_articles')
+
+
+class SearchListView(generic.ListView):
+    model = Article
+    template_name = 'news/index.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Article.objects.filter(
+            Q(title__icontains=query)
+            | Q(text__icontains=query)
+            | Q(author__first_name__icontains=query)
+            | Q(author__last_name__icontains=query)
+            | Q(comments__author__first_name__icontains=query)
+            | Q(comments__author__last_name__icontains=query)
+            | Q(comments__text__icontains=query)
+        )
