a0faacda07922e0b0d86eba54d3ebdb6209f115f
Andrei Lushpanov <a.lushpanov@wis.software>
2021-04-14 15:45:21 +0300

    Like added

--- notifications/models.py
+++ notifications/models.py
@@ -1,8 +1,20 @@
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
-
-from news.models import Article
 
 
 class Notification(models.Model):
     text = models.CharField(max_length=100)
-    article = models.ForeignKey(Article, related_name='notifications', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='notifications', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
