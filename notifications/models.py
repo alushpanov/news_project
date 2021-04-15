c3cc8d0b5e60b28c54a8cf58f0c2d39b4964432c
Andrei Lushpanov <a.lushpanov@wis.software>
2021-04-15 11:34:23 +0300

    content type removed from Notification, sender added

--- notifications/models.py
+++ notifications/models.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
@@ -7,7 +6,5 @@ from django.db import models
 class Notification(models.Model):
     text = models.CharField(max_length=100)
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='notifications', on_delete=models.CASCADE)
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey()
